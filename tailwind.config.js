394c2e6cb4b33297ec5f612ab02bbb6e879b7c73
ayamLollipop1 <ayamlollipop@outlook.com>
2025-01-01 13:34:10 +0000

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+const colors = require('tailwindcss/colors')
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["*"],
+  purge: {
+    content: ["./*.html"],
+  },
+  theme: {
+    extend: {
+      colors: {
+        orange: colors.orange,
+        teal: colors.teal,
+        "light-blue": colors.sky,
+      }
+    },
+  },
+  plugins: [],
+}
+
